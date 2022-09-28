e491af657fc8650ce185e46e5701898e5581a445
zhangke43 <zhangke43@meituan.com>
2022-09-28 14:51:56 +0800

    特征的重要性分析

--- FeatureAnalysis/FeatureImportance.py
+++ FeatureAnalysis/FeatureImportance.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 # 计算TopK个重要特征
-def CalTopkFeatures(ls, k=10, printf=False):
+def calTopkFeatures(ls, k=10, printf=False):
     """
     用于计算xgb模型前k个重要性特征并返回dataframe
     :param ls: 输入xgb的特征重要度json列表
@@ -27,7 +27,7 @@ def CalTopkFeatures(ls, k=10, printf=False):
 
 
 # 计算某个特征的重要度
-def CalOneFeature(ls, target_name):
+def calOneFeature(ls, target_name):
     """
     用于计算xgb模型某个特征的重要性排名和比例
     :param ls: 输入xgb的特征重要度json列表
@@ -47,3 +47,27 @@ def CalOneFeature(ls, target_name):
             break
     if not is_contain:
         print(f"输入的特征: '{target_name}' 不在特征重要度列表中, 这可能意味着该特征的重要度可能为零!（没有在建树中进行切分）")
+
+
+# 计算某批特征集的重要度排序
+def calBatchFeatures(ls, target_list):
+    """
+    用于计算某个特征集内特征的重要度
+    :param ls: 输入xgb的特征重要度json列表
+    :param target_list: 需要计算重要度的特征集合
+    :return: 空
+    """
+    target_lenth = len(target_list)
+    contain_list = []
+    print("输入特征集总数为: ", target_lenth)
+    for idx, row in enumerate(ls):
+        feature_name = row["feature"]
+        weight = row['weight']
+        rank = idx + 1
+        if feature_name in target_list:
+            print(f"特征名: '{feature_name}' , 排序: {rank} , 权重: {weight} .")
+            contain_list.append(feature_name)
+
+    zero_set = set(target_list) - set(contain_list)
+    for i in zero_set:
+        print(f"特征名: '{i}' , 排序: -1 , 权重: 0 .")
