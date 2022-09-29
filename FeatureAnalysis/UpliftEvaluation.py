7c8af0a7fc16085f27cdc4c1408632ac3728c3b7
zhangke43 <zhangke43@meituan.com>
2022-09-29 12:54:22 +0800

    uplift模型指标评估

--- FeatureAnalysis/UpliftEvaluation.py
+++ FeatureAnalysis/UpliftEvaluation.py
@@ -5,7 +5,7 @@ from pyspark.sql import SparkSession
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
 
 # 计算uplift最佳的差值系数
-def CalBestCoef(table_name, alpha_range=(0.1, 0.9), step=0.1):
+def CalBestCoef(table_name, alpha_range=(0.0, 1.0), step=0.05):
     """
     用于计算uplift模型中，score1 - alpha * score0 中的最佳alpha值（基于auc表现）
     :param table_name: 利用SQL开发处理后的得分预测hive表，包含[user_id, label, score1, score0]字段
@@ -20,11 +20,13 @@ def CalBestCoef(table_name, alpha_range=(0.1, 0.9), step=0.1):
             from {table_name}
             """
         )
-        Evaluator = BinaryClassificationEvaluator(labelCol="label", rawPredictionCol="uplift_score", metricName='areaUnderROC')
+        Evaluator = BinaryClassificationEvaluator(labelCol="label",
+                                                  rawPredictionCol="uplift_score",
+                                                  metricName='areaUnderROC')
         auc = Evaluator.evaluate(data)
         score_dict[alpha] = auc
 
-    score_dict = dict(sorted(score_dict.items(), key=lambda x: x[1], reverse=True))
-    return score_dict
+    res_dict = dict(sorted(score_dict.items(), key=lambda x: x[1], reverse=True))
+    return res_dict
 
 
