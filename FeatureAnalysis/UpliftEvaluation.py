5fda096034d305f9a081fcc0a8df4cc489a65f9e
zhangke43 <zhangke43@meituan.com>
2022-09-30 13:08:22 +0800

    uplift模型指标评估

--- FeatureAnalysis/UpliftEvaluation.py
+++ FeatureAnalysis/UpliftEvaluation.py
@@ -1,9 +1,3 @@
-import numpy as np
-import pandas as pd
-from pyspark import SparkConf
-from pyspark.sql import SparkSession
-from pyspark.ml.evaluation import BinaryClassificationEvaluator
-
 # 计算uplift最佳的差值系数
 def CalBestCoef(table_name, alpha_range=(0.0, 1.0), step=0.05):
     """
@@ -12,6 +6,10 @@ def CalBestCoef(table_name, alpha_range=(0.0, 1.0), step=0.05):
     :param alpha_range: 自定义alpha搜索范围
     :return:
     """
+    from pyspark import SparkConf
+    from pyspark.sql import SparkSession
+    from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
     score_dict = {}
     for alpha in np.arange(alpha_range[0], alpha_range[1], step):
         data = spark.sql(
@@ -30,3 +28,46 @@ def CalBestCoef(table_name, alpha_range=(0.0, 1.0), step=0.05):
     return res_dict
 
 
+# uplift评估指标1-分位数图表
+def EvalByDecilesGraph(exp_df, ctrl_df, k=10):
+    """
+    用于计算Uplift-decile-charts，通过观察柱状图正负情况，获得样本处于正效果的比例
+    :param exp_df: 实验组的dataframe，包含user_id, uplift_score, label
+    :param ctrl_df: 对照组的dataframe，包含user_id, uplift_score, label
+    :param k: 按等比例划分成的组数bins
+    :return: 包含bins和mean(uplift)的dataframe表
+    """
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    exp_df_desc = exp_df.sort_values(by='uplift_score', ascending=False)
+    exp_score = exp_df_desc.uplift_score.values
+    exp_group_num = len(exp_score) // k
+    exp_score_arr = np.zeros(k, dtype=np.float32)
+    idx = 0
+    for i in range(k):
+        group_mean = np.mean(exp_score[idx: idx + exp_group_num])
+        exp_score_arr[i] = group_mean
+        idx += exp_group_num + 1
+
+    ctrl_df_desc = ctrl_df.sort_values(by='uplift_score', ascending=False)
+    ctrl_score = ctrl_df_desc.uplift_score.values
+    ctrl_group_num = len(ctrl_score) // k
+    ctrl_score_arr = np.zeros(k, dtype=np.float32)
+    idx = 0
+    for i in range(k):
+        group_mean = np.mean(ctrl_score[idx: idx + ctrl_group_num])
+        ctrl_score_arr[i] = group_mean
+        idx += ctrl_group_num + 1
+
+    diff_mean_score = exp_score_arr - ctrl_score_arr
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(np.arange(k), diff_mean_score, width=0.5)
+    plt.title("S-model Uplift Decile approach")
+    plt.xlabel("Bins")
+    plt.ylabel("Mean of Uplift-Score")
+    plt.show()
+
+    return pd.DataFrame(diff_mean_score, columns=['mean_uplift'])
