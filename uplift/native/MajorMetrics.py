f035b66613d2cdc6172b2c6d41093678ce0d50d4
zhangke43 <zhangke43@meituan.com>
2022-10-17 15:30:06 +0800

    uplift模型指标评估

---
+++ uplift/native/MajorMetrics.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@project: Data-Science
+@Author: via
+@file: MajorMetrics.py
+@date: 2023/8/29 23:50
+@target: 
+"""
+
+import time
+import datetime
+import os
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm.autonotebook import tqdm
+
