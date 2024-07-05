4e2334473e0632ae056507a746ad983886f69e2c
viazhangke <viazhangke@didiglobal.com>
2024-07-05 17:25:55 +0800

    Tensorflow2定义mish激活函数

---
+++ tensorflow2/customer/layers/mish.py
@@ -0,0 +1,19 @@
+# 介绍文档：https://blog.csdn.net/ooooocj/article/details/136695008
+import tensorflow as tf
+
+# 基本公式与定义
+def mish(x):
+    return x * tf.tanh(tf.math.softplus(x))
+
+
+x = tf.random.uniform((10, 3), minval=-5, maxval=5, dtype=tf.float32)
+y = mish(x)
+print(x)
+print(y)
+
+# 在layer层中使用
+tf.keras.utils.get_custom_objects().update({'mish': mish})
+
+inputs = x
+out = tf.keras.layers.Dense(64, activation='mish')(inputs)
+print(out)
